e216e7ec65dfb90206b0711dd7088e9550250c9c
Queen-Alexa-Official <110767060+Queen-Alexa-Official@users.noreply.github.com>
2022-08-13 09:39:27 +0100

    Create index.js

---
+++ index.js
@@ -0,0 +1,8 @@
+return
+const bash = require("@ravindu01manoj/bash-js")
+const startFileWithNode = require("../indexstart.js")
+
+async function start() {
+	return await startFileWithNode(bash,"node manoj.js")
+}
+start()
